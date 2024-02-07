724583d3d1b90fee9a56f0140713eadd63871356
hanihoneykim <107341819+hanihoneykim@users.noreply.github.com>
2024-02-07 17:24:37 +0900

    edit: github client id

--- users/views.py
+++ users/views.py
@@ -144,7 +144,7 @@ class GithubLogIn(APIView):
         try:
             code = request.data.get("code")
             access_token = requests.post(
-                f"https://github.com/login/oauth/access_token?code={code}&client_id=1c83ba0e1bb2869d2f4c&client_secret={settings.GH_SECRET}",
+                f"https://github.com/login/oauth/access_token?code={code}&client_id=370b4196086b2cf0acc8&client_secret={settings.GH_SECRET}",
                 headers={"Accept": "application/json"},
             )
             access_token = access_token.json().get("access_token")
